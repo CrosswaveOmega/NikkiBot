3d0d61e259a2e6e39b94f3281ef17515949d707a
CrosswaveOmega <xtream2pro@gmail.com>
2023-07-12 14:51:57 -0400

    LastMinuteFix

--- purgpt/__init__.py
+++ purgpt/__init__.py
@@ -5,6 +5,6 @@
 api_key = None
 from .object_core import ApiCore
 from .object import Edit
-from .chat import ChatCompletion
+from .chat import ChatCreation
 from .api import PurGPTAPI
 import purgpt.error
