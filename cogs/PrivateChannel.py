749999cffb1077b8760166f528aac8b740076b73
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2023-11-15 11:01:01 -0500

    Create PrivateChannel.py

---
+++ cogs/PrivateChannel.py
@@ -0,0 +1,78 @@
+import asyncio
+import discord
+from discord.ext import commands, tasks
+import re
+
+from discord import app_commands
+
+from sqlitedict import SqliteDict
+
+from datetime import datetime, timezone
+import gui
+import utility
+from utility import MessageTemplates
+from bot import (
+    TCBot,
+    TCGuildTask,
+    Guild_Task_Functions,
+    StatusEditMessage,
+    TC_Cog_Mixin,
+)
+import numpy as np
+
+
+
+
+class PersonalServerConfigs(commands.Cog):
+    def __init__(self, bot):
+        self.helptext = (
+            "This is for personal server configuration.  Work in progress."
+        )
+        self.bot = bot
+        self.db = SqliteDict("./saveData/privateserverconfig.sqlite")
+        self.db.setdefault('guild_config',None)
+        
+
+    def cog_unload(self):
+        self.db.close()
+        self.userdb.close()
+
+    @commands.command()
+    @commands.is_owner()
+    @commands.guild_only()
+    async def enable_personal_config(self, ctx):
+        gid=f"g{ctx.guild.id}"
+        if self.db.get(gid)==None:
+            new={
+                "private_channels":{}
+            }
+            self.db.set(gid,new)
+            await ctx.send("Special config set up,")
+
+
+
+    @commands.command()
+    @commands.guild_only()
+    async def create_private_channel(self, ctx):
+        gid=f"g{ctx.guild.id}"
+        if self.db.get(gid)==None:
+            return
+        uid=f"u{ctx.author.id}"
+        existing_channel = self.db.get(gid)['private_channels'].get(uid,None)
+        
+        if existing_channel:
+            await ctx.send(f"You already have a private channel: {existing_channel.mention}")
+        else:
+            overwrites = {
+                ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                ctx.author: discord.PermissionOverwrite(read_messages=True, send_messages=True,manage_channels=True)
+            }
+            new_channel = await ctx.guild.create_text_channel(f"{ctx.author.name}s-channel", overwrites=overwrites)
+            await ctx.send(f"Private channel created: {new_channel.mention}")
+            self.db.get(gid)['private_channels'].set(uid,new_channel.id)
+            
+
+
+async def setup(bot):
+    pc = PersonalServerConfigs(bot)
+    await bot.add_cog(pc)
