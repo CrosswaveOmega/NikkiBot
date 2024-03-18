fef565ea4559898eebf20bf906534e65a013c13d
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2024-03-18 18:15:02 -0400

    Update GlobalCog.py

--- cogs/GlobalCog.py
+++ cogs/GlobalCog.py
@@ -25,7 +25,13 @@ from utility import MessageTemplates, RRuleView, formatutil
 from utility.embed_paginator import pages_of_embeds
 from utility import WebhookMessageWrapper as web
 from bot import TC_Cog_Mixin, super_context_menu, TCGuildTask, TCTaskManager
+import cogs.ResearchAgent as ra
+from discord.app_commands import checks, MissingPermissions
 
+def owner_only():
+  async def actual_check(interaction: discord.Interaction):
+    return await interaction.client.is_owner(interaction.user)
+  return app_commands.check(actual_check)
 
 class Global(commands.Cog, TC_Cog_Mixin):
     """General commands"""
@@ -34,11 +40,60 @@ class Global(commands.Cog, TC_Cog_Mixin):
         self.helptext = "Some assorted testing commands."
         self.bot = bot
         self.globalonly=True
-        
+
+    @app_commands.command(name="search", description="search the interwebs.")
+    @app_commands.describe(query="Query to search google with.")
+    async def websearch(self, interaction: discord.Interaction, query:str) -> None:
+        """get bot info for this server"""
+        ctx: commands.Context = await self.bot.get_context(interaction)
+        mess=await ctx.send("<a:LoadingBlue:1206301904863502337> Searching")
+        results = ra.tools.google_search(ctx.bot, query, 7)
+        allstr = ""
+        emb = discord.Embed(title="Search results", description='loading.')
+        readable_links = []
+
+        def indent_string(inputString, spaces=2):
+            indentation = " " * spaces
+            indentedString = "\n".join(
+                [indentation + line for line in inputString.split("\n")]
+            )
+            return indentedString
+
+        outputthis = f"### Search results for {query} \n\n"
+        for r in results["items"]:
+            desc = r.get("snippet", "NA")
+            allstr += r["link"] + "\n"
+            emb.add_field(
+                name=f"{r['title'][:200]}",
+                value=f"{r['link']}\n{desc}"[:1000],
+                inline=False,
+            )
+            outputthis += f"+ **Title: {r['title']}**\n **Link:**{r['link']}\n **Snippit:**\n{indent_string(desc,1)}"
+        await mess.edit(embed=emb)
+
+    @app_commands.command(name="supersearch", description="use db search.")
+    @app_commands.describe(query="Query to search DB for")
+    @owner_only
+    async def doc_talk(self, interaction: discord.Interaction, query:str) -> None:
+        """get bot info for this server"""
+        ctx: commands.Context = await self.bot.get_context(interaction)
+        mess=await ctx.send("<a:LoadingBlue:1206301904863502337> Searching")
+        try:
+            ans,source,_=await ra.actions.research_op(query, 9)
+            emb = discord.Embed(description=ans)
+            emb.add_field(name="source", value=str(source)[:1000], inline=False)
+            await mess.edit(content=None,embed=emb)
+        except Exception as e:
+            await ctx.send("something went wrong...")
+
+
+
     @app_commands.command(name="pingtest", description="ping")
     async def info(self, interaction: discord.Interaction) -> None:
         """get bot info for this server"""
         await interaction.response.send_message("Reading you loud and clear!")
+
+    
 
 async def setup(bot):
     await bot.add_cog(Global(bot))
