f0a3118988e49ed1de1c470fe61874759422de18
CrosswaveOmega <xtream2pro@gmail.com>
2024-04-10 13:25:40 -0400

    Update PalworldAPI.py

--- cogs/PalworldAPI.py
+++ cogs/PalworldAPI.py
@@ -7,26 +7,98 @@ from PIL import Image, ImageDraw
 from discord import app_commands
 import numpy as np
 # import datetime
-
+from utility.views import BaseView
 from bot import (
     TCBot,
     TC_Cog_Mixin,
 )
 from discord.ext import commands
 
-
+CELL_SIZE=200
 from discord.app_commands import Choice
 
+def draw_grid(filepath):
+    with Image.open(filepath).convert("RGBA") as img:
+        overlay2 = Image.new("RGBA", img.size, (0, 0, 0, 0))
+        draw2 = ImageDraw.Draw(overlay2)
+        cell_size = 200
+        width, height = img.size
+        for x in range(0, width, cell_size):
+            draw2.line([(x, 0), (x, height)], fill=(255, 255, 255, 60), width=1)
+        for y in range(0, height, cell_size):
+            draw2.line([(0, y), (width, y)], fill=(255, 255, 255, 60), width=1)
 
-class ToChoice(commands.Converter):
-    async def convert(self, ctx, argument):
-        if not ctx.interaction:
-            if type(argument) == str:
-                choice = Choice(name="fallback", value=argument)
-                return choice
-        else:
-            return argument
+        img = Image.alpha_composite(img, overlay2)
+    return img
+def highlight(img, coordinate):
 
+    overlay = Image.new("RGBA", img.size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(overlay)
+    draw.line(
+        [
+            (coordinate[0] + 1.5, coordinate[1] - 1),
+            (coordinate[0] + 1.5, coordinate[1] - 6),
+        ],
+        fill=(255, 0, 0, 200),
+        width=2,
+    )  # North
+    draw.line(
+        [
+            (coordinate[0] + 0.5, coordinate[1] + 2),
+            (coordinate[0] + 0.5, coordinate[1] + 7),
+        ],
+        fill=(255, 0, 0, 200),
+        width=2,
+    )  # South
+    draw.line(
+        [
+            (coordinate[0] - 1, coordinate[1] + 1.5),
+            (coordinate[0] - 6, coordinate[1] + 1.5),
+        ],
+        fill=(255, 0, 0, 200),
+        width=2,
+    )  # West
+    draw.line(
+        [
+            (coordinate[0] + 2, coordinate[1] + 0.5),
+            (coordinate[0] + 7, coordinate[1] + 0.5),
+        ],
+        fill=(255, 0, 0, 200),
+        width=2,
+    )  # East
+    draw.ellipse(
+        [
+            coordinate[0] - 1,
+            coordinate[1] - 1,
+            coordinate[0] + 2,
+            coordinate[1] + 2,
+        ],
+        outline=(255, 0, 0, 200),
+        width=1,
+    )
+    draw.ellipse(
+        [
+            coordinate[0] - 4,
+            coordinate[1] - 4,
+            coordinate[0] + 5,
+            coordinate[1] + 5,
+        ],
+        outline=(255, 0, 0, 200),
+        width=1,
+    )
+    img = Image.alpha_composite(img, overlay)
+    return img
+
+def crop_image(image, coordinate, off_by, cell_size=200):
+    ccr = coordinate
+    bc = ccr + off_by + np.array((1, 1))
+    uc = ccr - off_by
+    left = max(uc[0] * cell_size, 0)
+    top = max(uc[1] * cell_size, 0)
+    right = min(bc[0] * cell_size, image.width)
+    bottom = min(bc[1] * cell_size, image.height)
+    cropped_img = image.crop((left, top, right, bottom))
+    return cropped_img
 
 def capitalize_first_letter(string: str) -> str:
     return string.capitalize() if string else ""
@@ -34,6 +106,74 @@ def capitalize_first_letter(string: str) -> str:
 
 coor = app_commands.Range[int, -1000, 1000]
 msize = app_commands.Range[int, 1, 20]
+
+
+
+class MapViewer(BaseView):
+    """
+    scrollable palworld map.
+    """
+
+    def __init__(
+        self,
+        *,
+        user,
+        timeout=30 * 15,
+        img=None,
+        initial_coor=None,
+    ):
+        super().__init__(user=user, timeout=timeout)
+        self.value = False
+        self.done = None
+        self.img=img
+        self.focus_cell = np.array(initial_coor)//CELL_SIZE
+
+    def make_embed(self):
+        coors=f"Viewing cell {self.focus_cell[0]}, {self.focus_cell[1]}"
+        embed = discord.Embed(
+            description=f"Current pal map view.  \n{coors}"[:4000],
+            timestamp=discord.utils.utcnow(),
+        )
+        
+
+        cropped_img=crop_image(self.img,self.focus_cell,off_by=np.array((2,1)))
+        with io.BytesIO() as image_binary:
+            cropped_img.save(image_binary, "PNG")
+            image_binary.seek(0)
+            
+            embed.set_image(url=f"attachment://highlighted_palmap.png")
+            file = discord.File(fp=image_binary, filename="highlighted_palmap.png")
+
+        return embed, file
+
+    async def on_timeout(self) -> None:
+        self.value = "timeout"
+        self.stop()
+    @discord.ui.button(label="Left", style=discord.ButtonStyle.green, row=4)
+    async def move_left(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.focus_cell+=np.array((-1,0))
+        await interaction.response.edit_message(content="One moment...")
+        embed, file=self.make_embed()
+
+        await interaction.edit_original_response(content='',embed=embed, attachments=[file])
+
+    @discord.ui.button(label="Right", style=discord.ButtonStyle.green, row=4)
+    async def move_right(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.focus_cell+=np.array((1,0))
+        await interaction.response.edit_message(content="One moment...")
+        embed, file=self.make_embed()
+
+        await interaction.edit_original_response(content='',embed=embed, attachments=[file])
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.grey, row=4)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = False
+        await interaction.response.edit_message(content="Terminating.")
+        self.stop()
 
 
 class PalworldAPI(commands.Cog, TC_Cog_Mixin):
@@ -161,93 +301,15 @@ class PalworldAPI(commands.Cog, TC_Cog_Mixin):
         y2 = 1000 - y
 
 
-        def highlight_and_crop(filepath, coordinate, off_by):
-            with Image.open(filepath).convert("RGBA") as img:
-                overlay2 = Image.new("RGBA", img.size, (0, 0, 0, 0))
-                draw2 = ImageDraw.Draw(overlay2)
-                cell_size = 200
-                width, height = img.size
-                for x in range(0, width, cell_size):
-                    draw2.line([(x, 0), (x, height)], fill=(255, 255, 255, 60), width=1)
-                for y in range(0, height, cell_size):
-                    draw2.line([(0, y), (width, y)], fill=(255, 255, 255, 60), width=1)
+        
+        coordinate=(x2 * 2, y2 * 2)
+        img=draw_grid(file_path)
+        img=highlight(img, coordinate)
+        #cropped_img = crop_image(img,np.array(coordinate)//CELL_SIZE, np.array((3, 2)))
+        view=MapViewer(user=ctx.author,img=img,initial_coor=coordinate)
 
-                img = Image.alpha_composite(img, overlay2)
-                overlay = Image.new("RGBA", img.size, (0, 0, 0, 0))
-                draw = ImageDraw.Draw(overlay)
-                draw.line(
-                    [
-                        (coordinate[0] + 1.5, coordinate[1] - 1),
-                        (coordinate[0] + 1.5, coordinate[1] - 6),
-                    ],
-                    fill=(255, 0, 0, 200),
-                    width=2,
-                )  # North
-                draw.line(
-                    [
-                        (coordinate[0] + 0.5, coordinate[1] + 2),
-                        (coordinate[0] + 0.5, coordinate[1] + 7),
-                    ],
-                    fill=(255, 0, 0, 200),
-                    width=2,
-                )  # South
-                draw.line(
-                    [
-                        (coordinate[0] - 1, coordinate[1] + 1.5),
-                        (coordinate[0] - 6, coordinate[1] + 1.5),
-                    ],
-                    fill=(255, 0, 0, 200),
-                    width=2,
-                )  # West
-                draw.line(
-                    [
-                        (coordinate[0] + 2, coordinate[1] + 0.5),
-                        (coordinate[0] + 7, coordinate[1] + 0.5),
-                    ],
-                    fill=(255, 0, 0, 200),
-                    width=2,
-                )  # East
-                draw.ellipse(
-                    [
-                        coordinate[0] - 1,
-                        coordinate[1] - 1,
-                        coordinate[0] + 2,
-                        coordinate[1] + 2,
-                    ],
-                    outline=(255, 0, 0, 200),
-                    width=1,
-                )
-                draw.ellipse(
-                    [
-                        coordinate[0] - 4,
-                        coordinate[1] - 4,
-                        coordinate[0] + 5,
-                        coordinate[1] + 5,
-                    ],
-                    outline=(255, 0, 0, 200),
-                    width=1,
-                )
-                img = Image.alpha_composite(img, overlay)
-                ccr = np.array(coordinate)//cell_size     
-                
-                bc=ccr+off_by+np.array((1,1))
-                uc=(ccr-off_by)
-                left = max(uc[0]*cell_size, 0)
-                top = max(uc[1]*cell_size, 0)
-                right = min(bc[0]*cell_size, img.width)
-                bottom = min(bc[1]*cell_size, img.height)
-                cropped_img = img.crop((left, top, right, bottom))
-                cropped_img = img.crop((left, top, right, bottom))
-
-            return cropped_img
-
-        cropped_img = highlight_and_crop(file_path, (x2 * 2, y2 * 2), np.array((3,2)))
-
-        with io.BytesIO() as image_binary:
-            cropped_img.save(image_binary, "PNG")
-            image_binary.seek(0)
-            file = discord.File(fp=image_binary, filename="highlighted_palmap.png")
-        await mes.edit(content="done", attachments=[file])
+        emb,file=view.make_embed()
+        await mes.edit(content="done", attachments=[file],embed=emb,view=view)
 
 
 async def setup(bot):
