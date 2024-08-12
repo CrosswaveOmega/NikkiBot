b22b91584f749e8869cfb4b1669cdb3a96afe18f
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2024-08-12 12:09:16 -0400

    Update HelldiversCog.py

--- cogs/HelldiversCog.py
+++ cogs/HelldiversCog.py
@@ -863,7 +863,7 @@ class HelldiversCog(commands.Cog, TC_Cog_Mixin):
             if (((r+1) %3) == 0):
                 desc+="\n## "
             else:
-                desc+="`   `"
+                desc+="  "
             embed.add_field(name=f"Roll {r+1}",value=sload)
         embed.description=desc
         embed.set_author(name=f"Stratagem Roulette with {rolls} roll{'s' if rolls>1 else ''}")
