378a705d3b40bd0575d1dcb85eb337661318e868
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2024-08-17 16:21:50 -0400

    Update Reward.py

--- cogs/HD2/helldive/models/Reward.py
+++ cogs/HD2/helldive/models/Reward.py
@@ -2,8 +2,16 @@ from typing import *
 
 from pydantic import Field
 from .ABC.model import BaseApiModel
+from .ABC.utils import (
+    human_format as hf,
+    select_emoji as emj,
+    changeformatif as cfi,
+    extract_timestamp as et,
+)
+from discord.utils import format_dt as fdt
 
 
+    
 class Reward(BaseApiModel):
     """
     None model
@@ -16,3 +24,9 @@ class Reward(BaseApiModel):
     id32: Optional[int] = Field(alias="id32", default=None)
 
     amount: Optional[int] = Field(alias="amount", default=None)
+    
+    def format(self):
+        """Return the string representation of any reward."""
+        if self.type == 1:
+            return f"{emj('medal')} × {self.amount}"
+        return f"Unknown type:{self.type} × {self.amount}"
