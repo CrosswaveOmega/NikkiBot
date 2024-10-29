61fbfb0e340aaf98b6433e70478a6b7d49188004
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2024-10-29 12:57:22 -0400

    Update Tags2.py

--- cogs/Tags2.py
+++ cogs/Tags2.py
@@ -596,7 +596,7 @@ class Tags(commands.Cog):
                     embed.add_field(name=name, value=value, inline=False)
             if act:
                 embed_list.append(embed)
-        await utility.pages_of_embeds(ctx, embed_list)
+        await utility.pages_of_embeds(ctx, embed_list,ephemeral=True)
 
     @tags.command(name="get", description="get a tag by name")
     @app_commands.autocomplete(tagname=tag_autocomplete)
