972fcf2a860dd92872e857fa6dbe883f48ed8723
CrosswaveOmega <xtream2pro@gmail.com>
2025-09-12 13:05:43 -0400

    Update HelldiversCogAutoLog.py
    fixing error

--- cogs/HelldiversCogAutoLog.py
+++ cogs/HelldiversCogAutoLog.py
@@ -131,14 +131,18 @@ class Events:
         self.lastStatus: Dict[str, Any] = {}
         self.evt: List[GameEvent] = []
         self.trig: List[str] = []
+        self.hdml = ""
         self.ret = None
 
     def add_event(self, event: GameEvent, key: str) -> None:
         self.evt.append(event)
         if event.mode in [EventModes.NEW, EventModes.REMOVE]:
             self.ret = event.value.retrieved_at
+            if event.place == "news":
+                self.hdml += hdml_parse(event.value.message).replace("\n", " ") 
         elif event.mode == EventModes.CHANGE:
             self.ret = event.value[0].retrieved_at
+
 
         if key not in self.trig:
             self.trig.append(key)
@@ -199,6 +203,18 @@ class PlanetEvents(Events):
             self.lastInfo = _
             self.planet.waypoints = v.waypoints
             self.ret = v.retrieved_at
+
+    def add_event(self, event: GameEvent, key: str) -> None:
+        self.evt.append(event)
+        if event.mode in [EventModes.NEW, EventModes.REMOVE]:
+            self.ret = event["value"].retrieved_at
+            if event["place"] == "planetevents":
+                self.planet_event = event["value"]
+        elif event.mode == EventModes.CHANGE:
+            self.ret = event["value"][0].retrieved_at
+
+        if key not in self.trig:
+            self.trig.append(key)
 
 
 class SectorEvents(Events):
